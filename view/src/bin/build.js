0ae7066eba2e611a87cee14ae281c87e9d376ff7
Willyams Yujra <yracnet@gmail.com>
2021-05-06 13:08:44 -0400

    + PurgecssPlugin config for build-vs

--- view/src/bin/build.js
+++ view/src/bin/build.js
@@ -1,14 +1,24 @@
 #!/usr/bin/env node
-
 process.env.PUBLIC_URL = 'VSCODE_ROOT_URI/'
 process.env.BUILD_PATH = '../media'
 
 const rewire = require("rewire");
+const paths = require('react-scripts/config/paths.js');
 const defaults = rewire("react-scripts/scripts/build.js");
+const PurgecssPlugin = require('purgecss-webpack-plugin')
+const glob = require('glob-all')
+
 let config = defaults.__get__("config");
 
 config.optimization.splitChunks = false;
 config.optimization.runtimeChunk = false;
-//config.output.publicPath = '__URI_EXTENSION__/'
-//console.log('config', config);
-console.log('config.output.publicPath', config.output.publicPath);
+
+const sources = glob.sync(`${paths.appSrc}/**/*`, { nodir: true })
+const purgecssPlugin = new PurgecssPlugin({
+    paths: [
+        paths.appHtml,
+        ...sources
+    ]
+});
+const index = config.plugins.findIndex(it => it.constructor?.name === 'ManifestPlugin')
+config.plugins.splice(index + 1, 0, purgecssPlugin);
